d71d0a7db516afc549fb07e7d4962e592817f1b5
Messi-Cao <23141@burnside.school.nz>
2025-04-02 18:56:27 +1300

    Add testing1.py

---
+++ testing1.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+Database = 'cars.db'
+
+def print_every_car():
+    with sqlite3.connect(Database) as db:
+        cursor = db.cursor()
+        sql = "SELECT * FROM cars"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+
+        #print them nicely
+        print("The following are the list of chosen cars and their specifications:")
+        for car in results:
+            print(f"Name: {car[1]}") 
+            print(f"Top Speed: {car[2]}") 
+            print(f"Horsepower: {car[3]}")
+
+if __name__ == "__main__":
+    print_every_car()
