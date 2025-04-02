088b3aa494b40e781d7742c11edc9c52990575c3
Messi-Cao <23141@burnside.school.nz>
2025-04-02 19:32:14 +1300

    added print car models comment

--- testing.py
+++ testing.py
@@ -19,5 +19,7 @@ def print_every_car():
             print(f"Top Speed: {car[2]}") 
             print(f"Horsepower: {car[3]}")
 
+#print car models
+
 if __name__ == "__main__":
     print_every_car()
