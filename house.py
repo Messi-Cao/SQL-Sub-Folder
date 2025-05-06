697ef6633b323a988ef7801c480903bdca911878
Messi-Cao <23141@burnside.school.nz>
2025-05-06 14:40:40 +1200

    Improved "Custom" section on house.py

--- house.py
+++ house.py
@@ -138,6 +138,28 @@ def print_every_house_by_price():
             print(f"Street: {house[2]}")
             print(f"Average Cost: ${house[9]}")
 
+#define print_every_house_with_two_bedrooms
+def print_every_house_with_two_bedrooms():
+    '''print all the products nicely'''
+    with sqlite3.connect(Database) as db:
+        cursor = db.cursor()
+        sql = "SELECT * FROM house JOIN area ON house.area_id = area.id WHERE bedrooms = 2;"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        #loop through all the results from house
+        #print them nicely
+        print("The following are the list of two bedroom houses on auction in Pocket City:")
+        for house in results:
+            print(f"Address Number: {house[1]}") 
+            print(f"Street: {house[2]}") 
+            print(f"Bedrooms: {house[3]}")
+            print(f"Bathrooms: {house[4]}")
+            print(f"Floors: {house[5]}")
+            print(f"Basement: {house[6]}")
+            print(f"Description: {house[7]}")
+            print(f"Area Name: {house[11]}")
+            print(f"Average Cost: ${house[9]}")
+
 #Main code
 while True:
     user_input = input(
@@ -151,7 +173,8 @@ What would you like to do?
 6. Print all houses by address and description
 7. Print all houses by address and area
 8. Print all houses by address and average cost
-9. Quit
+9. Choose from custom list (WIP)
+0. Quit
 """)
     if user_input == "1":
         print_every_house()
@@ -170,6 +193,99 @@ What would you like to do?
     elif user_input == "8":
         print_every_house_by_price()
     elif user_input == "9":
+        #Custom selection
+        while True:
+            try: 
+                custom_input = input(
+"""
+Please choose from the following:
+1. Bedrooms
+2. Bathrooms
+3. Floors
+4. Basement Availability
+5. Quit
+""")
+                #bedroom quantity
+                if custom_input == "1":
+                    while True:
+                        bedroom_quantity = input("Choose your bedroom quantity (To exit, please press '0'): ")
+                        if bedroom_quantity == "2":
+                            print()
+                        elif bedroom_quantity == "3":
+                            print()
+                        elif bedroom_quantity == "4":
+                            print()
+                        elif bedroom_quantity == "5":
+                            print()
+                        elif bedroom_quantity == "6":
+                            print()
+                        elif bedroom_quantity == "7":
+                            print()
+                        elif bedroom_quantity == "0":
+                            break
+                        else:
+                            print('Sorry, we currently do not have this option. Please come back after!')
+                #bathroom quantity
+                elif custom_input == "2":
+                    while True:
+                        bathroom_quantity = input("Choose your bathroom quantity (To exit, please press '0'): ")
+                        if bathroom_quantity == "1":
+                            print()
+                        elif bathroom_quantity == "2":
+                            print()
+                        elif bathroom_quantity == "2.5":
+                            print()
+                        elif bathroom_quantity == "3":
+                            print()
+                        elif bathroom_quantity == "4.5":
+                            print()
+                        elif bathroom_quantity == "5":
+                            print()
+                        elif bathroom_quantity == "10":
+                            print()
+                        elif bathroom_quantity == "0":
+                            break
+                        else:
+                            print('Sorry, we currently do not have this option. Please come back after!')
+                #floor quantity
+                elif custom_input == "3":
+                    while True: 
+                        floor_quantity = input("Choose your floor quantity (To exit, please press '0'): ")
+                        if floor_quantity == "1":
+                            print()
+                        elif floor_quantity == "2":
+                            print()
+                        elif floor_quantity == "3":
+                            print()
+                        elif floor_quantity == "4":
+                            print()
+                        elif floor_quantity == "0":
+                            break
+                        else:
+                            print('Sorry, we currently do not have this option. Please come back after!')
+                #basement availability
+                elif custom_input == "4":
+                    basement_availability = input("Choose 'yes' or 'no' for your basement availability (To exit, please press '0'): ")
+                    if basement_availability == "yes":
+                        print()
+                    elif basement_availability == "no":
+                        print()
+                    elif basement_availability == "0":
+                        break
+                    else:
+                        print("You may only choose 'yes' or 'no' for this option, please try again.")
+                #To get out of the selection
+                elif custom_input == "5":
+                    break
+                #Invalid input message
+                else:
+                    print('Oops, invalid input! Please try again!')
+            #Another invalid input message
+            except ValueError: 
+                print('Oops, invalid input! Please try again!')
+    #break out of code
+    elif user_input == "0":
         break
+    #unavailable option message
     else:
         print('Oops, this option is not available. Please try another one!')
